a782197f5acab66b2abd146a1b791f3c7f33e082
semanda <patash248@gmail.com>
2024-06-02 06:49:12 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <Navbar/>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: 'Home' },
+    { id: 2, text: ' Team' },
+    { id: 3, text: 'Services' },
+    { id: 4, text: 'About' },
+    { id: 5, text: 'Contact' },
+  ];
+
+  return (
+    <div className='bg-black flex justify-between items-center h-24  px-4 text-white'>
+      {/* Logo  text-[#00dfba]*/}
+      
+      <img src="/logo3.svg" alt="" width={25} />
+      <h1 className='w-full text-md font-semibold ml-2 '>Bukenya,Chemonges & Co.</h1>
+      
+      {/* Desktop Navigation */}
+      <ul className='hidden md:flex'>
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className='block md:hidden'>
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className='w-full text-xl font-bold text-[#00df9a] m-4'>BUKENYA LAW</h1>
+
+        {/* Mobile Navigation Items */}
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
+
