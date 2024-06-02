5311e09a6b433ae8cd7fd1e8e46c0dfb07c6978c
semanda <patash248@gmail.com>
2024-06-02 07:27:10 +0200

    add page routings

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/index";
+import Team from "./pages/Team/index"
+import Services from "./pages/Services/index"
+import Contact from "./pages/Contact/index"
+import About from "./pages/About/index"
 
 import './App.css'
 import Navbar from './components/Navbar'
@@ -7,7 +13,18 @@ function App() {
 
 
   return (
-   <Navbar/>
+    < >
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  </>
   )
 }
 
---
+++ src/pages/Contact/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>contactpage</div>
+  )
+}
+
+export default index
